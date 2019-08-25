45bfd5ac84d7491ba31d7994faa1e45a134975ba
NourAdel <noura7305@gmail.com>
2019-08-25 20:56:54 +0200

    main logic

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,23 @@
 import json
 
 data = json.load(open("data.json"))
-print(data["doctor"])
+
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+
+
+    else:
+        print("Couldn't find the word. Please check it again.")
+        return
+
+
+Input = input("Enter the Word: \n ")
+output = translate(Input)
+print(output)
