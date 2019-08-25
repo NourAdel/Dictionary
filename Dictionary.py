32eaca412f40f4def313e6e61b2c1427099edf52
NourAdel <noura7305@gmail.com>
2019-08-25 21:25:56 +0200

    finding the closest match

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -11,6 +12,17 @@ def translate(word):
         return data[word.title()]
     elif word.upper() in data:
         return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        print("did you mean %s? y OR n " % get_close_matches(word, data.keys())[0])
+        ans=input()
+        if ans == 'y':
+            return data[get_close_matches(word, data.keys())[0]]
+        elif ans == 'n':
+            print("Couldn't find the word. Please check it again.")
+            return
+        else:
+            print("invalid option")
+            return
 
 
     else:
@@ -20,4 +32,8 @@ def translate(word):
 
 Input = input("Enter the Word: \n ")
 output = translate(Input)
-print(output)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
